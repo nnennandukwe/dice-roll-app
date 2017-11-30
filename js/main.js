fbf7b2900f1e15b5f964c0cb78dda65b4224adb5
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2017-11-30 10:05:31 -0500

    primary structure and js logic of dice game

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+$("#getAnswer").on("click",function(){
+  var userDiceSides = $("#diceSides").val()
+  var userNumberOfDice = $("#numberOfDice").val()
+  
+  while(userNumberOfDice > 0){
+     var answer = Math.floor((Math.random() * userDiceSides)+1)
+     console.log(answer)
+     userNumberOfDice --;
+  }
+})
+
+function rollDice(numOfDice,numOfSides){
+  
+  while(numOfDice>0){
+    var answer = Math.floor(Math.random()*numOfSides+1)
+    // console.log(answer)
+    document.getElementsByTagName("span")[0].innerHTML = answer;
+    numOfDice--;
+  }
+}
+});
